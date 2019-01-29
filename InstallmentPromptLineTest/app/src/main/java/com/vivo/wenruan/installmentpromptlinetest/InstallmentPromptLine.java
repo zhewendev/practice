732913460673e1fa9dbfc90e2e7964b40942fae2
zhewen <zhewen.dev@gmail.com>
2019-01-29 11:50:03 +0800

    2019.1.29:InstallmentPromptLineTest 优化

--- InstallmentPromptLineTest/app/src/main/java/com/vivo/wenruan/installmentpromptlinetest/InstallmentPromptLine.java
+++ InstallmentPromptLineTest/app/src/main/java/com/vivo/wenruan/installmentpromptlinetest/InstallmentPromptLine.java
@@ -24,12 +24,13 @@ public class InstallmentPromptLine extends View {
     private static final int DEFAULT_FAILED_DRAWABLE_ID = R.drawable.icon_circle;
     private static final int DEFAULT_UNDERWEAR_DRAWABLE_ID = R.drawable.icon_exclamation_mark;
 
-    private int mRadiu;
-    private int mLineLength;
-    private int mSectionTextSize;
     private int mGeneralDrawableResourceId;
     private int mFailedDrawableResourceId;
     private int mUnderwayDrawableResourceId;
+    private int mRadiu;
+    private int mLineLength;
+    private int mSectionTextSize;
+    private boolean mShowSig;
     private int mSignColor;
     private int mSignTextSize;
     private int mSignTextColor;
@@ -38,32 +39,27 @@ public class InstallmentPromptLine extends View {
     private int mSignArrowHeight;
     private int mSignArrowWidth;
     private int mSignRound;
-    private int mSignSpace = 10;
-//    private int mTextSpace = 35;
-    private boolean mShowSig = true;
+    private int mSignSpace = 20;
     private int mSignOffset = 20;
 
     private Bitmap mIconGeneral;
     private Bitmap mIconUnderway;
     private Bitmap mIconFailed;
-    private Paint mCompletePaint;
+
     private List<InstallmentPromptLineBean> mData = new ArrayList<>();
     private int mWidth; //宽度
     private int mHeight;//高度
     private Rect mGeneralRect;
     private Rect mUnderwayRect;
     private Rect mFailedRect;
+    private RectF mSignBoundsRectF;
+    private Paint mPaint;
     private Paint mIconPaint;
     private Paint mSignPaint;
-    private Paint mSignTextPaint;
-    private RectF mSignBoundsRectF;
     private Point point1;
     private Point point2;
     private Point point3;
     private Path mTrianglePath;
-    private Rect mSignTextRect;
-
-
 
     public InstallmentPromptLine(Context context) {
         this(context, null);
@@ -76,16 +72,17 @@ public class InstallmentPromptLine extends View {
     public InstallmentPromptLine(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.InstallmentPromptLine);
-        mRadiu = typedArray.getInt(R.styleable.InstallmentPromptLine_common_radius, DEFAULT_RADIU);
-        mSectionTextSize = typedArray.getDimensionPixelSize(R.styleable.InstallmentPromptLine_common_section_text_size, sp2px(context, 14));
         mGeneralDrawableResourceId = typedArray.getResourceId(R.styleable.InstallmentPromptLine_common_icon_underway,DEFAULT_GENERAL_DRAWABLE_ID);
         mFailedDrawableResourceId = typedArray.getResourceId(R.styleable.InstallmentPromptLine_common_icon_failed,DEFAULT_FAILED_DRAWABLE_ID);
         mUnderwayDrawableResourceId = typedArray.getResourceId(R.styleable.InstallmentPromptLine_common_icon_general,DEFAULT_UNDERWEAR_DRAWABLE_ID);
+        mRadiu = typedArray.getInt(R.styleable.InstallmentPromptLine_common_radius, DEFAULT_RADIU);
+        mSectionTextSize = typedArray.getDimensionPixelSize(R.styleable.InstallmentPromptLine_common_section_text_size, sp2px(context, 14));
+        mShowSig = typedArray.getBoolean(R.styleable.InstallmentPromptLine_common_show_sign, false);
         mSignColor = typedArray.getColor(R.styleable.InstallmentPromptLine_common_sign_color, ContextCompat.getColor(context, R.color.colorPrimary));
         mSignTextSize = typedArray.getDimensionPixelSize(R.styleable.InstallmentPromptLine_common_sign_text_size, sp2px(context,14));
         mSignTextColor = typedArray.getColor(R.styleable.InstallmentPromptLine_common_sign_text_color,ContextCompat.getColor(context, R.color.colorAccent));
         mSignHeight = typedArray.getDimensionPixelSize(R.styleable.InstallmentPromptLine_common_sign_height, dp2px(context, 30));
-        mSignWidth = typedArray.getDimensionPixelSize(R.styleable.InstallmentPromptLine_common_sign_width, dp2px(context, 60));
+        mSignWidth = typedArray.getDimensionPixelSize(R.styleable.InstallmentPromptLine_common_sign_width, dp2px(context, 120));
         mSignArrowHeight = typedArray.getDimensionPixelSize(R.styleable.InstallmentPromptLine_common_sign_arrow_height, dp2px(context, 3));
         mSignArrowWidth = typedArray.getDimensionPixelSize(R.styleable.InstallmentPromptLine_common_sign_arrow_width,dp2px(context, 5));
         mSignRound = typedArray.getDimensionPixelSize(R.styleable.InstallmentPromptLine_common_sign_round, dp2px(context, 3));
@@ -99,10 +96,10 @@ public class InstallmentPromptLine extends View {
         mIconFailed = BitmapFactory.decodeResource(getResources(), mFailedDrawableResourceId);
         mIconPaint = new Paint(Paint.ANTI_ALIAS_FLAG);  //抗锯齿
         mIconPaint.setStyle(Paint.Style.FILL);
-        mCompletePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mCompletePaint.setColor(Color.WHITE);
-        mCompletePaint.setStrokeWidth(4);   //线宽
-        mCompletePaint.setTextSize(mSectionTextSize);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.WHITE);
+        mPaint.setStrokeWidth(4);   //线宽
+        mPaint.setTextSize(mSectionTextSize);
         mSignPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mSignPaint.setStyle(Paint.Style.FILL);
         mSignPaint.setAntiAlias(true);
@@ -111,17 +108,13 @@ public class InstallmentPromptLine extends View {
         mGeneralRect = new Rect(0, 0, mIconGeneral.getWidth(), mIconGeneral.getHeight());
         mUnderwayRect = new Rect(0, 0, mIconUnderway.getWidth(), mIconUnderway.getHeight());
         mFailedRect = new Rect(0, 0, mIconFailed.getWidth(), mIconFailed.getHeight());
+
         mSignBoundsRectF = new RectF();
         point1 = new Point();
         point2 = new Point();
         point3 = new Point();
         mTrianglePath = new Path();
         mTrianglePath.setFillType(Path.FillType.EVEN_ODD);
-        mSignTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mSignTextPaint.setStyle(Paint.Style.FILL);
-        mSignTextPaint.setTextSize(mSignTextSize);
-        mSignTextPaint.setColor(mSignTextColor);
-
 
     }
 
@@ -136,8 +129,10 @@ public class InstallmentPromptLine extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        mWidth = w;
-        mHeight = h;
+        mWidth = getPaddingLeft();
+        mHeight = getPaddingTop();
+
+
     }
 
     @Override
@@ -151,12 +146,12 @@ public class InstallmentPromptLine extends View {
         if (mData == null || mData.size() == 0) {
             return;
         }
-        int centerX = mRadiu;
+        int centerX = mWidth + mRadiu;
         int centerY;
         if (mShowSig) {
-            centerY = mSignHeight + mSignSpace + mRadiu;
+            centerY = mHeight + mSignHeight + mSignSpace + mRadiu;
         } else {
-            centerY = mRadiu;
+            centerY =mHeight + mRadiu;
         }
         for (int i = 0; i < mData.size(); i++) {
             InstallmentPromptLineBean item = mData.get(i);
@@ -167,14 +162,14 @@ public class InstallmentPromptLine extends View {
                             mIconFailed, item.getStatus() == InstallmentPromptLineBean.StepStatus.GENERAL ? mGeneralRect :
                     item.getStatus() == InstallmentPromptLineBean.StepStatus.UNDERWAY ? mUnderwayRect :
                             mFailedRect, srcBitmapRect, mIconPaint);
-            float v = mCompletePaint.measureText(item.getName());
+            float v = mPaint.measureText(item.getName());
 
-            canvas.drawText(item.getName(), bitmapCenter - v / 2, centerY + mRadiu * 2, mCompletePaint);
+            canvas.drawText(item.getName(), bitmapCenter - v / 2, centerY + mRadiu * 2, mPaint);
 
             if (item.getLineLength() != 0) {
                 int lineLeft = centerX + mRadiu;
                 mLineLength = item.getLineLength();
-                canvas.drawLine(lineLeft, centerY - 2, lineLeft + mLineLength, centerY - 2, mCompletePaint);
+                canvas.drawLine(lineLeft, centerY - 2, lineLeft + mLineLength, centerY - 2, mPaint);
             }
             if (item.getIsShowSign()){
                 drawSign(canvas, mSignHeight, centerX);
@@ -184,16 +179,30 @@ public class InstallmentPromptLine extends View {
         }
     }
 
+    /**
+     * 绘制提示框
+     * @param canvas 画布
+     * @param mSignHeight 提示框高度
+     * @param centerX 节点中心坐标
+     */
     private void drawSign(Canvas canvas, int mSignHeight, int centerX) {
-        mSignBoundsRectF.set(centerX - mSignOffset, 0, centerX + mSignWidth - mSignOffset, mSignHeight);
+        mSignBoundsRectF.set(centerX - mSignOffset, mHeight, centerX + mSignWidth - mSignOffset, mSignHeight + mHeight);
         canvas.drawRoundRect(mSignBoundsRectF, mSignRound, mSignRound, mSignPaint);
-        point1.set(centerX - mSignArrowWidth / 2, mSignHeight);
-        point2.set(centerX + mSignArrowWidth / 2, mSignHeight);
-        point3.set(centerX, mSignHeight + mSignArrowHeight);
+        point1.set(centerX - mSignArrowWidth / 2, mSignHeight + mHeight);
+        point2.set(centerX + mSignArrowWidth / 2, mSignHeight + mHeight);
+        point3.set(centerX, mSignHeight + mSignArrowHeight + mHeight);
         drawTriangle(canvas, point1, point2, point3, mSignPaint);
 
     }
 
+    /**
+     * 绘制提示框上三角形
+     * @param canvas
+     * @param point1
+     * @param point2
+     * @param point3
+     * @param paint
+     */
     private void drawTriangle(Canvas canvas, Point point1, Point point2, Point point3, Paint paint) {
         mTrianglePath.reset();
         mTrianglePath.moveTo(point1.x, point1.y);
@@ -204,20 +213,41 @@ public class InstallmentPromptLine extends View {
         canvas.drawPath(mTrianglePath, paint);
     }
 
+    /**
+     * 绘制提示框中文字
+     * @param canvas
+     * @param centerX
+     * @param item
+     */
     private void drawSignText(Canvas canvas,int centerX, InstallmentPromptLineBean item) {
         String str = item.getSignContent();
-        mSignTextRect = new Rect();
-        mSignTextPaint.getTextBounds(str, 0, str.length(), mSignTextRect);
-        float baseline = Math.abs(mSignTextRect.top);
-        canvas.drawText(str, centerX, baseline, mSignTextPaint);
+        Rect mSignTextRect = new Rect();
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setTextSize(mSignTextSize);
+        mPaint.setColor(mSignTextColor);
+        mPaint.getTextBounds(str, 0, str.length(), mSignTextRect);
+        float baseline = Math.abs(mSignTextRect.top) + mHeight + (mSignHeight - mSignTextRect.height()) / 2;
+        canvas.drawText(str, centerX, baseline, mPaint);
 
     }
 
+    /**
+     * sp转换为px
+     * @param context
+     * @param spValue
+     * @return
+     */
     public int sp2px(Context context, float spValue) {
         final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
         return (int) (spValue * fontScale + 0.5f);
     }
 
+    /**
+     * dp转换为px
+     * @param context
+     * @param dpValue
+     * @return
+     */
     public int dp2px(Context context, float dpValue) {
         float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
