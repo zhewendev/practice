d99df464592c047e8cfba87c751ef28014536b01
ruanwen <11085273@bbktel.com>
2020-10-12 22:40:37 +0800

    [D201012]新增kotlin工程

---
+++ Kotlin/KotlinPracticeThird/app/src/main/java/com/zhewen/kotlinpracticethird/Person.kt
@@ -0,0 +1,12 @@
+package com.zhewen.kotlinpracticethird
+
+class Person {
+    override fun equals(other: Any?): Boolean {
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int {
+        return super.hashCode()
+
+    }
+}
